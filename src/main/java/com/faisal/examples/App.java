acf34ebf23d998ebd33c76796e77a8a583a60320
lloisborn <faisalmorensya@gmail.com>
2018-05-03 11:59:37 +0700

    creating consumer

--- src/main/java/com/faisal/examples/App.java
+++ src/main/java/com/faisal/examples/App.java
@@ -2,8 +2,7 @@ package com.faisal.examples;
 
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) {
         System.out.println( "Hello World!" );
     }
 }
